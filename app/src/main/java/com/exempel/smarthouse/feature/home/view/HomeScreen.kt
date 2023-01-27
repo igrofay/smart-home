5426e7850e3635daf712e6aeb41d535364d4b430
sergejnikitin <igrofay@gmail.com>
2023-01-27 16:20:27 +0500

    proj

--- app/src/main/java/com/exempel/smarthouse/feature/home/view/HomeScreen.kt
+++ app/src/main/java/com/exempel/smarthouse/feature/home/view/HomeScreen.kt
@@ -3,9 +3,11 @@ package com.exempel.smarthouse.feature.home.view
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -37,7 +39,9 @@ fun HomeScreen(
             onDismissRequest = { visibleDialogMap = false },
         ) {
             MapView(
-                modifier = Modifier.size(320.dp)
+                modifier = Modifier
+                    .size(320.dp)
+                    .clip(RoundedCornerShape(10))
             ){ geo->
                 geo.geoObject.boundingBox?.northEast?.let {
                     viewModel.onEvent(EventHome.SelectedPointOnMap(it))
